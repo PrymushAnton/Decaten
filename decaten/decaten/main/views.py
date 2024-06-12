cba9994463d945e11ce3b396fcc8e2b04697c7a1
Andrii Druzhha <druzgaandrey@gmail.com>
2024-06-12 20:58:40 +0300

    Add product card

--- decaten/decaten/main/views.py
+++ decaten/decaten/main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Carusel
+from catalog_product.models import Product
 
 # Create your views here.
 def main(request):
     context = {}
     images_for_carusel = Carusel.objects.all()
+    all_products = Product.objects.all()
     context['images_for_carusel'] = images_for_carusel
+    context['all_products'] = all_products
     return render(request, 'main/main.html', context)
