68d84dbc0081e042b9128cf4196d69caccc120c6
Andrii Druzhha <druzgaandrey@gmail.com>
2024-06-14 19:46:52 +0300

    Create product page

---
+++ decaten/decaten/catalog_product/static/catalog_product/js/product.js
@@ -0,0 +1,4 @@
+function clickBtn(id, url) {
+    $('img.w-100').attr('src', url);
+    $('input[name="id_flaur"]').attr('value', id);
+};
--- decaten/decaten/decaten/urls.py
+++ decaten/decaten/decaten/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     path('catalog/filter_products/', filter_products, name='filter_products'),
     path('catalog/get_flavour_image/', get_flavour_image, name='get_flavour_image'),
     path('catalog/product_image/', product_image, name='product_image'),
+    path('product/<id>', product_page, name='product_page'),
 ]
 
 if DEBUG:
